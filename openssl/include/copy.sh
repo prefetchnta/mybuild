ebaf54eaf3f97923f1471de7704f92ea75d39605
prefetchnta <crhackos@gmail.com>
2017-11-08 18:18:26 +0800

    release openssl 1.0.2m for android

---
+++ openssl/include/copy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+PWD=`pwd`
+for link in $(find ./ -type l)
+do
+  echo
+  echo "Link is:"
+  echo $link
+  loc=$(dirname $link)
+  dir=$(readlink -f $link)
+  echo "location is:"
+  echo $loc
+  echo "dir is:"
+  echo $dir
+  rm $link
+  cp $dir $link -rf
+done

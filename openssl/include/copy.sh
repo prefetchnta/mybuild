360ac06782f480327056814b9908804a015514fa
prefetchnta <crhackos@gmail.com>
2018-05-17 09:31:52 +0800

    update shell

--- openssl/include/copy.sh
+++ openssl/include/copy.sh
@@ -1,17 +1,16 @@
-#!/bin/bash
+#!/bin/sh
 
-PWD=`pwd`
 for link in $(find ./ -type l)
 do
-  echo
-  echo "Link is:"
-  echo $link
-  loc=$(dirname $link)
-  dir=$(readlink -f $link)
-  echo "location is:"
-  echo $loc
-  echo "dir is:"
-  echo $dir
-  rm $link
-  cp $dir $link -rf
+    echo
+    echo "Link is:"
+    echo $link
+    loc=$(dirname $link)
+    dir=$(readlink -f $link)
+    echo "location is:"
+    echo $loc
+    echo "dir is:"
+    echo $dir
+    rm $link
+    cp $dir $link -rf
 done

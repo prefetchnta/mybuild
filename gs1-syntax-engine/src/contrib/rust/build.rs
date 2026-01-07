fae843181cbdd4fbab72d6fdc0fcf9d32260d5d0
prefetchnta <crhackos@gmail.com>
2026-01-07 11:15:13 +0800

    delete useless files

--- gs1-syntax-engine/src/contrib/rust/build.rs
+++
@@ -1,5 +0,0 @@
-fn main() {
-    println!("cargo:rustc-link-lib=gs1encoders");
-    println!("cargo:rustc-link-search=../../c-lib/build");
-    println!("cargo:rustc-env=LD_LIBRARY_PATH=../../c-lib/build");
-}
